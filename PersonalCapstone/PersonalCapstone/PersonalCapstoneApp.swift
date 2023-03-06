215c0719122399c197d0ed156039c168509b90a8
Michael Whiting <michaelwhiting@AirPods-Pro.local>
2023-03-06 08:44:41 -0700

    Initial commit

---
+++ PersonalCapstone/PersonalCapstone/PersonalCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalCapstoneApp.swift
+//  PersonalCapstone
+//
+//  Created by Michael Whiting on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PersonalCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
